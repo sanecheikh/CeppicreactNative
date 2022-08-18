ff9ec1342f4134bd234c1ba499d4b71640a32011
sane cheikh oumar <safietou2717@gmail.com>
2022-08-18 14:51:55 +0200

    ajouter application

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View ,TextInput} from 'react-native';
+import { Button } from 'react-native-web';
+
 
 export default function App() {
+  const [lienTexteSaisie,SetLientexteSaisie] = useState("");
+  const [listeLiens,setlistelien]= useState([]);
+  function ajoutLienhandler(){
+    //console.log(lienTexteSaisie);
+    setListeLiens ([...lienTexteSaisie]);
+  }
+  function LienInputhandler(textSaisie){
+    console.log(textSaisie);
+    SetLientexteSaisie(textSaisie);
+  }
   return (
     <View style={styles.container}>
-      <Text>ceppic  react native </Text>
-      <StatusBar style="auto" />
+      <View style= {styles.inputContainer}>  
+       <TextInput placeholder='=nouveau ...'
+       style={styles.inputLien} onChangeText=
+       {LienInputhandler}/>
+     
+      <Button title="Ajouter un lien" onPress= {ajoutLienhandler}/>
+      
+    </View>
+    <view>
+      {listeLiens.map((lien) =><text key ={lien}>{lien}</text>)}
+    </view>
+    <View style = { styles.liencontainer}>
+    <Text>Liste des lien..</Text>
+    </View>
     </View>
   );
 }
@@ -17,4 +41,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputLien: {
+    borderColor: "grey",
+    borderWidth:1,
+    padding:8,
+    width:"70%"
+
+  },
+  inputContainer:{
+    flex:1,
+    flexDirection: "row",
+    alignItems:'center',
+  },
+  liencontainer:{
+    flex:3
+  }
+
+
+ 
+
 });
